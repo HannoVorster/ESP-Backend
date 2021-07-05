4ee72fbefb7a5e065c986e43d3169af192f752f9
Hanno Vorster <vorster.hanno@protonmail.com>
2021-07-05 21:24:35 +0200

    Add dev/prod database conn strings

--- connect.php
+++ connect.php
@@ -3,10 +3,17 @@
     header("Access-Control-Allow-Methods: PUT, GET, POST");
     header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 
+    // DEV...
     define('SERVER_NAME', "localhost");
     define('USER_NAME', "root");
     define('PASSWORD', "Boomsakalaka1");
     define('DB_NAME', "esp_dev");
+
+    // PROD...
+    /*define('SERVER_NAME', "localhost");
+    define('USER_NAME', "hannolwt_admin");
+    define('PASSWORD', "w~BQ[F.duXw3");
+    define('DB_NAME', "hannolwt_spad262");*/
 
 	function connect() {
         $connect = mysqli_connect(SERVER_NAME, USER_NAME, PASSWORD, DB_NAME);
